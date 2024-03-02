940c065b3bac444bf7fa41230d5309a130fc0af4
Uname50 <158527626+Uname50@users.noreply.github.com>
2024-03-01 23:10:29 -0500

    added solution

--- 121_Best_Time_For_Stonks.py
+++ 121_Best_Time_For_Stonks.py
@@ -6,6 +6,21 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 """
 
+import operator 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        pass
+    def maxProfit(self, prices: list[int]) -> int:
+        
+        # track the max profits
+        max_ending_here, max_so_far = 0, 0 
+        
+        # iterate over the daily profits 
+        for profit in map(operator.sub, prices[1:], prices):
+            
+            # update the higher value between the current max plus new profit or 0
+            max_ending_here = max(max_ending_here + profit, 0)
+            
+            # update the overall max profit 
+            max_so_far = max(max_so_far, max_ending_here)
+        
+        # return the max profit
+        return max_so_far
