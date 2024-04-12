2e3ab43e8d790cf1102e37b62b57830c6ea31829
Uname50 <158527626+Uname50@users.noreply.github.com>
2024-04-11 20:38:06 -0400

    problem 122 solution

---
+++ 122_Best_time_for_Stonks_2.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/ 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum(
+
+            # Calculate the difference between consecutive days prices
+            curr - prev for prev, curr in zip(prices, prices[1:]) 
+
+            # Include only positive differences, indicating profit
+            if curr - prev > 0
+        )
