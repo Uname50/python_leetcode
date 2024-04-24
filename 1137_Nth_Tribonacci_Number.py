227a9644b26706278489b06098be239176e7149e
Dmitrii I <estaquito@outlook.com>
2024-04-23 23:33:20 -0400

    puzzle 1137 solution

--- 1137_Nth_Tribonacci_Number.py
+++ 1137_Nth_Tribonacci_Number.py
@@ -1,4 +1,24 @@
-# test
+# https://leetcode.com/problems/n-th-tribonacci-number/?envType=daily-question&envId=2024-04-24
 
-# test conncetion 
-print("hello world!")
+def tribonacci(n):
+    
+    # Base cases that are defined in the description
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    
+    # The given values of the first 3 values
+    t0, t1, t2 = 0, 1, 1
+    
+    # Calculate up to Tn 
+    for i in range(3, n+1):
+
+        # The current Tribonacci number is the sum of the previous 3 numbers
+        current = t0 + t1 + t2
+
+        # Shift the previous three numbers
+        t0, t1, t2 = t1, t2, current
+        
+    # Return the nth Tribonacci number
+    return t2  
