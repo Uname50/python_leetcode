acec71c440a51b2ec8f392eff99c62e03a87f1f3
Dmitrii I <estaquito@outlook.com>
2024-05-13 19:41:12 -0400

    puzzle 21 init commit

---
+++ 21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: # type: ignore
